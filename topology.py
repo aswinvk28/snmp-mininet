4f17498c86751dd16f39933592d4eec3d4db222c
Aswin Vijayakumar <aswinvk28@gmail.com>
2016-11-18 16:34:23 +0000

    bind address, socket close

--- topology.py
+++ topology.py
@@ -5,12 +5,17 @@ import snmpload
 HOST = "mininet"               # Symbolic name meaning all available interfaces
 PORT = 8001              # Arbitrary non-privileged port
 s = socket.socket(socket.AF_INET, socket.SOCK_TCP, socket.IPPROTO_IP)
+bind_address = (HOST, PORT)
+s.bind(bind_address) # bind to the agent
+s.listen(1) # listen to any incoming requests
 
-connection, bind_address = s.accept()
+connection, address = s.accept()
 
 while 1:
     data = connection.recv(1024)
+    if not data break
     list = data.split("\t")
     mininetSNMPValues = mininet_snmp_load(list[0], list[1], list[2]) # returns a dictionary
     connection.send(str(mininetSNMPValues))
 
+connection.close()
